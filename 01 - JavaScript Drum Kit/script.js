4702ac5bb27b1b0b6dc9df43957cdc990352a6fc
Chris Wehner <chrisw7128@gmail.com>
2023-01-15 11:56:17 -0800

    second commit

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,6 +1,18 @@
-document.addEventListener("keydown", function (e) {
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if (!audio) return; // stop the function from running all together
-  console.log(audio);
+  audio.currentTime = 0; //rewind to the start
   audio.play();
-});
+  key.classList.add("playing");
+}
+
+document.addEventListener("keydown", playSound);
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return; //skip it if not a transform
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
